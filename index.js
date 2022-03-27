90705651f40c1c9d74d65863c0082632ddc25dc0
Edward Angelo Salas <zyro670@gmail.com>
2022-03-27 16:04:58 -0400

    commit tree 837b2d8d80264fa31a8e7b7f8284b0752ff65ee2

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5].reduce(add, 0);
 // Code your solution here
+var totalBatteries = batteryBatches;
+function add(accumulator, a) {
+    return accumulator + a;
+  }
